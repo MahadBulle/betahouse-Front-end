70cd792a4075c059bc16cf0e111f78334ee27a24
MahadBulle <mahadhussein012@gmail.com>
2023-08-01 03:49:52 -0700

    about change

--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -1,146 +1,183 @@
-import { Box, Stack, IconButton, Typography, Alert, TextField, Button, Divider } from "@mui/material"
-import AddHomeWorkIcon from '@mui/icons-material/AddHomeWork';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import { useEffect, useState } from "react";
-import AboutList from "./AboutList";
-import { AddAbout, getAllAbout, UpdateAbout } from "./ApiCrudsabout";
-import { useForm } from "react-hook-form";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+// import { Box, Stack, IconButton, Typography, Alert, TextField, Button, Divider } from "@mui/material"
+// import AddHomeWorkIcon from '@mui/icons-material/AddHomeWork';
+// import Dialog from '@mui/material/Dialog';
+// import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogTitle from '@mui/material/DialogTitle';
+// import { useEffect, useState } from "react";
+// import AboutList from "./AboutList";
+// import { AddAbout, getAllAbout, UpdateAbout } from "./ApiCrudsabout";
+// import { useForm } from "react-hook-form";
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from 'yup';
 
-const AboutSchema = yup.object({
-  Description: yup.string().required('Enter the description'),
-  ShortDescription: yup.string().required("Enter the short description")
+// const AboutSchema = yup.object({
+//   Description: yup.string().required('Enter the description'),
+//   ShortDescription: yup.string().required("Enter the short description")
 
-});
+// });
 
-export const About = () => {
-  const { register, handleSubmit, reset, setValue, formState: { errors } } = useForm()
-  const [dailogOpen, setDailog] = useState(false)
-  const ToggleDailog = () => {
-    setDailog(!dailogOpen)
-  }
+// export const About = () => {
+//   const { register, handleSubmit, reset, setValue, formState: { errors } } = useForm()
+//   const [dailogOpen, setDailog] = useState(false)
+//   const ToggleDailog = () => {
+//     setDailog(!dailogOpen)
+//   }
 
-  const [About, setAbout] = useState([])
-  const [abid, setabid] = useState('')
-  const [ time, setTime]= useState(new Date())
+//   const [About, setAbout] = useState([])
+//   const [abid, setabid] = useState('')
+//   const [ time, setTime]= useState(new Date())
 
-  useEffect(() => {
-    const allAbout = async () => {
+//   useEffect(() => {
+//     const allAbout = async () => {
 
-      const { data } = await getAllAbout()
+//       const { data } = await getAllAbout()
 
-      console.log('Xogta', data)
+//       console.log('Xogta', data)
 
-      setAbout(data)
-    }
+//       setAbout(data)
+//     }
 
-    allAbout()
-
-
-  }, [time])
+//     allAbout()
 
 
+//   }, [time])
 
 
-  const AddNewAbout = async (data) => {
-    if (abid !== '') {
-
-      try {
-        await UpdateAbout(abid, data)
-        console.log("Data has been Updated successfully")
-        ToggleDailog()
-        reset()
-        setTime(new Date())
-        toast.success(data.message);
-      } catch (err) {
-        console.log("error ayaa jira ", err)
-        toast.error(data.message);
-
-      }
-    }
-    else {
-      try {
-        await AddAbout(data)
-        console.log("Data has been saved successfully")
-        ToggleDailog()
-        reset()
-        setTime(new Date())
-        toast.success(data.message);
-      } catch (err) {
-        console.log("error ayaa jira ", err)
-        toast.error(data.message);
-
-      }
-
-    }
-
-  }
 
 
-  const UpdateAboutInfo = async (data)=>{
-    // console.log("xogta la rabbo in la update gareeyo",data)
-    setValue("Description", data.Description)
-    setValue("ShortDescription", data.ShortDescription)
-    setabid(data._id)
-    ToggleDailog()
+//   const AddNewAbout = async (data) => {
+//     if (abid !== '') {
+
+//       try {
+//         await UpdateAbout(abid, data)
+//         console.log("Data has been Updated successfully")
+//         ToggleDailog()
+//         reset()
+//         setTime(new Date())
+//         toast.success(data.message);
+//       } catch (err) {
+//         console.log("error ayaa jira ", err)
+//         toast.error(data.message);
+
+//       }
+//     }
+//     else {
+//       try {
+//         await AddAbout(data)
+//         console.log("Data has been saved successfully")
+//         ToggleDailog()
+//         reset()
+//         setTime(new Date())
+//         toast.success(data.message);
+//       } catch (err) {
+//         console.log("error ayaa jira ", err)
+//         toast.error(data.message);
+
+//       }
+
+//     }
+
+//   }
+
+
+//   const UpdateAboutInfo = async (data)=>{
+//     // console.log("xogta la rabbo in la update gareeyo",data)
+//     setValue("Description", data.Description)
+//     setValue("ShortDescription", data.ShortDescription)
+//     setabid(data._id)
+//     ToggleDailog()
     
-    }
+//     }
 
-  return <>
-    <Box p={4}>
+//   return <>
+//     <Box p={4}>
 
-      <Alert severity="info">About Us</Alert>
-      <Box sx={{ display: "flex", justifyContent: "space-between" }} my={4}>
-        <Typography variant="h6">List About</Typography>
+//       <Alert severity="info">About Us</Alert>
+//       <Box sx={{ display: "flex", justifyContent: "space-between" }} my={4}>
+//         <Typography variant="h6">List About</Typography>
 
-        <IconButton onClick={ToggleDailog}>
-          <AddHomeWorkIcon sx={{ color: "green" }} />
-        </IconButton>
-      </Box>
+//         <IconButton onClick={ToggleDailog}>
+//           <AddHomeWorkIcon sx={{ color: "green" }} />
+//         </IconButton>
+//       </Box>
 
-      <Dialog open={dailogOpen} onClose={ToggleDailog}>
-        <DialogTitle>New Image</DialogTitle>
-        <Box component={"form"} onSubmit={handleSubmit(AddNewAbout)}>
-          <DialogContent>
-            <Box sx={{ width: "400px" }} mt={2}>
-
-
-
-              <Stack spacing={2} direction={'column'}>
+//       <Dialog open={dailogOpen} onClose={ToggleDailog}>
+//         <DialogTitle>New Image</DialogTitle>
+//         <Box component={"form"} onSubmit={handleSubmit(AddNewAbout)}>
+//           <DialogContent>
+//             <Box sx={{ width: "400px" }} mt={2}>
 
 
 
-                <TextField label="Description" {...register("Description")} variant="outlined" size="small" fullWidth 
+//               <Stack spacing={2} direction={'column'}>
+
+
+
+//                 <TextField label="Description" {...register("Description")} variant="outlined" size="small" fullWidth 
                 
-                />
+//                 />
 
-                <TextField label="ShortDescription" variant="outlined" {...register("ShortDescription")} size="small" fullWidth />
+//                 <TextField label="ShortDescription" variant="outlined" {...register("ShortDescription")} size="small" fullWidth />
 
 
-              </Stack>
+//               </Stack>
 
-            </Box>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={ToggleDailog}>Cancel</Button>
-            <Button variant="contained" type="submit" size="small">Submit</Button>
+//             </Box>
+//           </DialogContent>
+//           <DialogActions>
+//             <Button onClick={ToggleDailog}>Cancel</Button>
+//             <Button variant="contained" type="submit" size="small">Submit</Button>
 
-          </DialogActions>
+//           </DialogActions>
 
-        </Box>
-        <ToastContainer></ToastContainer>
-      </Dialog>
+//         </Box>
+//         <ToastContainer></ToastContainer>
+//       </Dialog>
 
-      <Divider />
-      {About ? <AboutList AboutData={About} update={UpdateAboutInfo} /> : null}
+//       <Divider />
+//       {About ? <AboutList AboutData={About} update={UpdateAboutInfo} /> : null}
 
-    </Box>
-  </>
+//     </Box>
+//   </>
+// }
+// export default About
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { Box, Stack, IconButton, Typography, Alert, TextField, Button, Divider } from "@mui/material"
+import "./About.css"
+
+export default function About() {
+  const [fahfahin, setfahfahin] = useState('');
+  const [fahfahinyer, setfahfahinyer] = useState('');
+const Update = ()=>{
+  // console.log("datada",fahfahin)
+  const data = {
+    Description: fahfahin,
+    ShortDescription: fahfahinyer
+  }
+  console.log("xogta",data)
 }
-export default About
+
+  return (
+    <div>
+     <Button onClick={()=>{
+      Update()
+     }
+    }>
+      update
+     </Button>
+    <Box sx={{ p:4 }}>
+      <ReactQuill className='rc'  theme="snow" value={fahfahin} onChange={setfahfahin} />;
+    </Box>
+
+    <Box sx={{ p:4 }}>
+      <ReactQuill className='rc'  theme="snow" value={fahfahinyer} onChange={setfahfahinyer} />;
+    </Box>
+
+    </div>
+  )
+}
